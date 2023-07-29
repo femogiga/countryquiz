a9de967c3d3cc0aa5d58a19d168267ae5d358427
femogiga <femogiga@yahoo.com>
2023-07-29 14:51:46 +0100

    style issue solved

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,16 +1,50 @@
-const Button = ({ countryText ,letter,onClick,id}) => {
+import { useEffect, useState } from "react"
+
+const Button = ({ countryText, letter, onClick, id, disabled, right, wrong }) => {
+        const[clicked,setClicked]=useState(false)
+
+
+
+
+
+    console.log('right===>', right)
+    console.log('wrong===>', wrong)
 
     return (
+        // <div className="button flow-1">
+        //     <button className={`flex flex-center ${right ? "right-answer-style" : ""} ${wrong ? "wrong-answer-style" : ""} `} onClick={onClick} id={id} disabled={disabled}>
+        //         <span className="flex col-gap-1">
+        //             <span className='letter'>{letter}</span>
+        //             {countryText}
+        //         </span>
+
+        //         {wrong && <span className={`material-symbols-outlined ${wrong} && wrong`}> cancel
+        //         </span>}
+        //         {right && <span className={`right material-symbols-outlined right`}>
+        //             check_circle
+        //         </span>
+        //         }
+        //     </button>
+        // </div>
+
         <div className="button flow-1">
-            <button className='flex flex-center' onClick={onClick} id={id}>
-                <span className="flex col-gap-1">
-                    <span className='letter'>{letter}</span>
-                    {countryText}
-                </span>
-                <span className="material-symbols-outlined"> cancel
-                </span>
-            </button>
-        </div>
+        <button className={`flex flex-center`} onClick={onClick} id={id} disabled={disabled}>
+            <span className="flex col-gap-1">
+                <span className='letter'>{letter}</span>
+                {countryText}
+            </span>
+
+            {wrong && <span className={`material-symbols-outlined `}> cancel
+            </span>}
+            {right && <span className={`material-symbols-outlined`}>
+                check_circle
+            </span>
+            }
+        </button>
+    </div>
+
+
+
     )
 }
 
--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -22,11 +22,17 @@ const Card = (children) => {
     const [optionOne, setOptionOne] = useState({})
     const [optionTwo, setOptionTwo] = useState({})
     const [optionThree, setOptionThree] = useState({})
-    const [past, setPast] = useState([])
+    const [score, setScore] = useState(0)
     const [mapped, setMapped] = useState([])
+    const [answerState, setAnswerState] = useState(false)
+    const [right, setRight] = useState(false)
+    const [wrong, setWrong] = useState(false)
+    const [array, setArray] = useState()
+    const [style, setStyle] = useState('')
+
     const ref = useRef(null)
 
-    let array = []
+    //let array = []
     useEffect(() => {
         // set current question and remove question from main data array
 
@@ -39,20 +45,24 @@ const Card = (children) => {
         if (data.length > 0) {
 
 
-            setCurrent(data[randomIndex1])
+            setCurrent(() => data[randomIndex1])
             setOptionOne(() => data[randomIndex2]);
             setOptionTwo(() => data[randomIndex3]);
             setOptionThree(() => data[randomIndex4]);
 
             //array sort randomize the order to which the answers appear
-            array.sort(() => Math.random() - 0.5)
+            // array.sort(() => Math.random() - 0.5)
             console.log('newArry', array)
             console.log('current', current)
             console.log('option', optionOne)
             console.log('option', optionTwo)
             console.log('option', optionThree)
-            setMapped(array)
+            const array1 = [current, optionOne, optionTwo, optionThree]
+            array1.sort(() => Math.random() - 0.5)
+            setMapped(array1)
             // console.log('mapped', mapped)
+
+
 
         }
         else {
@@ -66,28 +76,54 @@ const Card = (children) => {
     //array variable creates a normal array
     //and is set to mapped for the mapping of answers
 
-    array = [optionOne, optionTwo, optionThree, current]
+    //array = [optionOne, optionTwo, optionThree, current]
     const letter = ["A", "B", "C", "D"]
 
     const handleNext = (e) => {
-        e.preventDefault()
+        const buttons = document.querySelectorAll('.button button');
+        buttons.forEach((button) => {
+            button.classList.remove('right-answer-style', 'wrong-answer-style');
+        });
+
         const filtered = data.filter(item => item?.name?.common !== current?.name?.common)
         setData(filtered)
+        setAnswerState(false)
+        e.preventDefault()
+        setRight(false)
+        setWrong(false)
 
+        return
     }
 
+
+
+    //ref from forwardRef on Question component
+    // id compared to the ref innerText if the component
     const handleAnswer = (e) => {
         console.log(current?.capital)
+
         e.preventDefault()
+        e.target.classList.remove('right-answer-style')
+        e.target.classList.remove('wrong-answer-style')
         if (e.target.id === ref.current.innerText) {
             // console.log(e.target.id + " ==== " + ref.current.innerText)
             console.log('true')
+            e.target.classList.add('right-answer-style')
+            setScore(score => score + 1)
+            setAnswerState(true)
+            console.log("score", score)
+            setRight(() => true)
+            setWrong(() => false)
             return
         }
         else {
-            // console.log(e.target.id + " ==== " + ref.current.innerText)
-            // console.log('target',current?.capital)
+            setRight(() => false)
+            setWrong(() => true)
+            setAnswerState(true)
             console.log('false')
+            console.log("score", score)
+            e.target.classList.add('wrong-answer-style')
+            e.target.classList.remove('right-answer-style')
             return;
         }
 
@@ -109,7 +145,7 @@ const Card = (children) => {
 
                 <Question key='question' questionText={mapped[random.int(0, 3)]?.capital} value={mapped[random.int(0, 3)]} ref={ref} />
                 {
-                    mapped.map((answer, index) => (<Button id={answer?.capital} key={index} letter={letter[index]} countryText={answer?.name?.common} onClick={(e) => handleAnswer(e)} value={answer?.name?.common} />))
+                    mapped.map((answer, index) => (<Button id={answer?.capital} className wrong={wrong} right={right} disabled={answerState} key={index} letter={letter[index]} countryText={answer?.name?.common} onClick={(e) => handleAnswer(e)} value={answer?.name?.common} />))
                 }
 
                 <Next onClick={(e) => handleNext(e)} />
