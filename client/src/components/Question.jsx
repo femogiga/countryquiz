d78803d9df1bf27b338a7eb14d5204c45f59962a
femogiga <femogiga@yahoo.com>
2023-08-01 19:31:11 +0100

    question type removed taet from text

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -2,15 +2,15 @@ import { useRef } from "react"
 import { forwardRef } from "react"
 
 
-const Question = forwardRef(function Question({ questionText, flag,questionType }, ref) {
+const Question = forwardRef(function Question({ questionText, flag, questionType }, ref) {
 
     return (
         <div className='flow-2'>
             {
                 questionType ?
-                    <h2><span className = 'hide' ref={ref}>{questionText}</span>Which country does the flag belong to?</h2>
+                    <h2><span disabled className='hide' ref={ref}>{questionText}</span>Which country does the flag belong to?</h2>
                     :
-                    <h2><span ref={ref}>{questionText}</span> is the capital of</h2>
+                    <h2><span disabled ref={ref}>{questionText}</span> is the capital of</h2>
             }
         </div>
     )
